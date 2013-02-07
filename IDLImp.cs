d8b35112a2d336609620f0cf26841844e869a103
vmas <vmas@localhost>
2013-02-07 16:17:56 +0400

    Add batch mode and solution file

--- IDLImp.cs
+++ IDLImp.cs
@@ -19,6 +19,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 
@@ -31,12 +32,16 @@ namespace SIL.FieldWorks.Tools
 	/// ----------------------------------------------------------------------------------------
 	public class IDLImpConsole
 	{
+		private static string[] _args;
+
 		private static void ShowHelp()
 		{
 			System.Console.WriteLine("\nIDLImporter. Creates .NET interfaces from an IDL file.");
 			System.Console.WriteLine("Copyright (c) 2002-2007, SIL International. All Rights Reserved.\n");
-			System.Console.WriteLine("Syntax: {0} [options] file.idl", 
-				Path.GetFileName(Application.ExecutablePath));
+			System.Console.WriteLine("Syntax:\n");
+			System.Console.WriteLine("{0} [options] file.idl", Path.GetFileName(Application.ExecutablePath));
+			System.Console.WriteLine("{0} [options] path", Path.GetFileName(Application.ExecutablePath));
+			System.Console.WriteLine();
 			System.Console.WriteLine("possible options:");
 			System.Console.WriteLine("\t/o outfile\tname of created file (Default: file.cs)");
 			System.Console.WriteLine("\t/c configfile\tname of XML configuration file ({0}.xml)",
@@ -59,6 +64,7 @@ namespace SIL.FieldWorks.Tools
 		[STAThread]
 		public static int Main(string[] args)
 		{
+			_args = args;
 			bool fOk = true;
 			try
 			{
@@ -68,35 +74,36 @@ namespace SIL.FieldWorks.Tools
 					return 0;
 				}
 
-				// Get all necessary file names
-				List<string> usingNamespaces = new List<string>();
 				string sFileName = args[args.Length - 1];
-				string sXmlFile = Path.ChangeExtension(Application.ExecutablePath, "xml");
-				string sOutFile = Path.ChangeExtension(sFileName, "cs");
-				string sNamespace = Path.GetFileNameWithoutExtension(sFileName);
+				string sDirectory = Directory.Exists(sFileName) ? sFileName : null;
+				string sOutFile = null;
+				string sXmlFile = null;
+				string sNamespace = null;
+				// Get all necessary file names
+				List<string> usingNamespaces = new List<string>();				
 				StringCollection idhFiles = new StringCollection();
 				StringCollection refFiles = new StringCollection();
 				bool fCreateComments = true;
 
-				for (int i = 0; i < (args.Length-1)/2; i++)
+				for (int i = 0; i < (args.Length - 1) / 2; i++)
 				{
-					switch (args[i*2])
+					switch (args[i * 2])
 					{
 						case "/o":
 						case "-o":
-							sOutFile = args[i*2+1];
+							sOutFile = args[i * 2 + 1];
 							break;
 						case "/c":
 						case "-c":
-							sXmlFile = args[i*2+1];
+							sXmlFile = args[i * 2 + 1];
 							break;
 						case "/n":
 						case "-n":
-							sNamespace = args[i*2+1];
+							sNamespace = args[i * 2 + 1];
 							break;
 						case "/u":
 						case "-u":
-							usingNamespaces.Add(args[i*2+1]);
+							usingNamespaces.Add(args[i * 2 + 1]);
 							break;
 						case "/?":
 						case "-?":
@@ -104,7 +111,7 @@ namespace SIL.FieldWorks.Tools
 							return 0;
 						case "/x":
 						case "-x":
-							fCreateComments = (args[i*2+1] == "0" ? false : true);
+							fCreateComments = (args[i * 2 + 1] == "0" ? false : true);
 							break;
 						case "/i":
 						case "-i":
@@ -115,28 +122,80 @@ namespace SIL.FieldWorks.Tools
 							refFiles.Add(args[i * 2 + 1]);
 							break;
 						default:
-							Console.WriteLine("\nWrong parameter: {0}\n", args[i*2]);
+							Console.WriteLine("\nWrong parameter: {0}\n", args[i * 2]);
 							ShowHelp();
 							return 0;
 					}
 				}
 
-				Console.WriteLine("Generating {0}...", Path.GetFileName(sOutFile));
+				sXmlFile = sXmlFile ?? Path.ChangeExtension(Application.ExecutablePath, "xml");
 
 				IDLImporter imp = new IDLImporter();
-				fOk = imp.Import(usingNamespaces, sFileName, sXmlFile, sOutFile, sNamespace,
-					idhFiles, refFiles, fCreateComments);
+				fOk = true;
+				if (sDirectory == null)
+				{
+					sOutFile = sOutFile ?? Path.ChangeExtension(sFileName, "cs");
+					sNamespace = sNamespace ?? Path.GetFileNameWithoutExtension(sFileName);
+
+					Console.WriteLine("Generating {0}...", Path.GetFileName(sOutFile));
+					fOk = imp.Import(usingNamespaces, sFileName, sXmlFile, sOutFile, sNamespace,
+						idhFiles, refFiles, fCreateComments);
+				}
+				else
+				{
+					foreach (string srcFileName in Directory.GetFiles(sDirectory, "*.idl"))
+					{
+						fOk = OnImport(imp, usingNamespaces, srcFileName, sXmlFile,
+							Path.ChangeExtension(srcFileName, "cs"),
+							sNamespace ?? Path.GetFileNameWithoutExtension(srcFileName),
+							idhFiles, refFiles, fCreateComments);
+					}
+				}
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
 				System.Console.WriteLine("Internal program error in program {0}", e.Source);
-				System.Console.WriteLine("\nDetails:\n{0}\nin method {1}.{2}\nStack trace:\n{3}", 
+				System.Console.WriteLine("\nDetails:\n{0}\nin method {1}.{2}\nStack trace:\n{3}",
 					e.Message, e.TargetSite.DeclaringType.Name, e.TargetSite.Name, e.StackTrace);
 
 				return 1;
 			}
 
 			return fOk ? 0 : 2;
+		}
+
+		private static bool OnImport(IDLImporter importer, List<string> usingNamespaces, string sFileName,
+			string sXmlFile, string sOutFile, string sNamespace, StringCollection idhFiles, StringCollection refFiles, bool fCreateComments)
+		{
+			Console.WriteLine("Generating {0}...", Path.GetFileName(sOutFile));
+
+			importer.ResolveBaseType += importer_ResolveBaseType;
+			bool fOk = importer.Import(usingNamespaces, sFileName, sXmlFile, sOutFile, sNamespace,
+				idhFiles, refFiles, fCreateComments);
+			importer.ResolveBaseType -= importer_ResolveBaseType;
+			return fOk;
+		}
+
+		private static void importer_ResolveBaseType(object sender, ResolveBaseTypeEventArgs e)
+		{
+			var args = new List<string>(_args);
+			int last = args.Count - 1;
+			args[last] = Path.Combine(args[last], e.TypeName + ".idl");
+			if (File.Exists(args[last]))
+			{
+				var startInfo = new ProcessStartInfo();
+				startInfo.FileName = typeof(IDLImpConsole).Assembly.Location;
+				startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+				startInfo.Arguments = string.Join(" ", args);
+				startInfo.RedirectStandardOutput = true;
+				startInfo.UseShellExecute = false;
+				using (var process = Process.Start(startInfo))
+				{
+
+					process.WaitForExit();
+					Console.WriteLine(process.StandardOutput.ReadToEnd());
+				}
+			}
 		}
 	}
 }
---
+++ IDLImporter/ResolveBaseTypeEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SIL.FieldWorks.Tools
+{
+	public class ResolveBaseTypeEventArgs: EventArgs
+	{
+		public ResolveBaseTypeEventArgs(string typeName, CodeNamespace nameSpace)
+		{
+			TypeName = typeName;
+			NameSpace = nameSpace;
+		}
+
+		public string TypeName { get; private set; }
+		public CodeNamespace NameSpace { get; set; }
+	}
+}
